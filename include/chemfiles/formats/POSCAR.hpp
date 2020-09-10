929d1084a9c1270d95ebe2c07c8d9aacc50f92d4
Seaton Ullberg <seatonullberg@gmail.com>
2020-09-10 16:54:04 -0400

    Add skeleton code for POSCAR

--- include/chemfiles/formats/POSCAR.hpp
+++ include/chemfiles/formats/POSCAR.hpp
@@ -6,6 +6,7 @@
 
 #include <cstdint>
 #include <string>
+#include <map>
 #include <memory>
 
 #include "chemfiles/File.hpp"
@@ -30,6 +31,44 @@ public:
     void read_next(Frame& frame) override;
     void write_next(const Frame& frame) override;
     optional<uint64_t> forward() override;
+
+private:
+    /// Read the top-line comment into a property
+    void read_comment(Frame& frame);
+    /// Read the unit cell and associated scaling factor
+    void read_unit_cell(Frame& frame);
+    /// Read the optional chemical symbols and required type counts
+    void read_species(Frame& frame);
+    /// Read optional selective dynamics declaration
+    void read_selective_dynamics(Frame& frame);
+    /// Read binary cartesian/direct declaration
+    void read_cartesian_direct(Frame& frame);
+    /// Read all atomic properties
+    void read_atoms(Frame& frame);
+
+    /// Write the comment line
+    void write_comment(const Frame& frame);
+    /// Write the unit cell and scale factor
+    void write_unit_cell(const Frame& frame);
+    /// Write the optional chemical species and their counts
+    void write_species(const Frame& frame);
+    /// Write the optional selective dynamics declaration
+    void write_selective_dynamics(const Frame& frame);
+    /// Write the cartesian/direct declaration
+    void write_cartesian_direct(const Frame& frame);
+    /// Write the atomic data 
+    void write_atoms(const Frame& frame);
+
+    // =============== Data used for reading files
+    /// Flag indicating selective dynamics
+    bool has_selective_dynamics_ = false;
+    /// Mapping of chemical symbols to their counts
+    std::map<std::string, int> species_;
+    /// Coordinate system types
+    enum coordinate_t {
+        CARTESIAN,
+        DIRECT
+    } coordinate_system_;
 };
 
 template<> FormatInfo format_information<POSCARFormat>();
--- src/formats/POSCAR.cpp
+++ src/formats/POSCAR.cpp
@@ -24,8 +24,52 @@ template<> FormatInfo chemfiles::format_information<POSCARFormat>() {
     );
 }
 
-void POSCARFormat::read_next(Frame& frame) {}
+void POSCARFormat::read_next(Frame& frame) {
+    if (file_.tellpos() != 0) {
+        throw format_error("POSCAR format only supports reading one frame");
+    }
+    // TODO
+}
 
-void POSCARFormat::write_next(const Frame& frame) {}
+void POSCARFormat::write_next(const Frame& frame) {
+    if (file_.tellpos() != 0) {
+        throw format_error("POSCAR format only supports writing one frame");
+    }
+    // TODO
+}
 
-optional<uint64_t> POSCARFormat::forward() {}
+optional<uint64_t> POSCARFormat::forward() {
+    // POSCAR only supports one step, so always act like there is only one
+    auto position = file_.tellpos();
+    if (position == 0) {
+        // advance pointer for the next call
+        file_.readline();
+        return position;
+    } else {
+        return nullopt;
+    }
+}
+
+void POSCARFormat::read_comment(Frame& frame) {}
+
+void POSCARFormat::read_unit_cell(Frame& frame) {}
+
+void POSCARFormat::read_species(Frame& frame) {}
+
+void POSCARFormat::read_selective_dynamics(Frame& frame) {}
+
+void POSCARFormat::read_cartesian_direct(Frame& frame) {}
+
+void POSCARFormat::read_atoms(Frame& frame) {}
+
+void POSCARFormat::write_comment(const Frame& frame) {}
+
+void POSCARFormat::write_unit_cell(const Frame& frame) {}
+
+void POSCARFormat::write_species(const Frame& frame) {}
+
+void POSCARFormat::write_selective_dynamics(const Frame& frame) {}
+
+void POSCARFormat::write_cartesian_direct(const Frame& frame) {}
+
+void POSCARFormat::write_atoms(const Frame& frame) {}
